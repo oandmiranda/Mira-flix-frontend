d207e4b8b3b0a1911d808803d0e9490a1b98769b
andersonMiranda <and.miranda1818@gmail.com>
2025-01-13 17:28:30 -0300

    test(MenuCategory): ensures that the component render the props title

--- src/components/Logo/Logo.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Logo from './Logo';
-
-describe('Logo', () => {
-  it('should show the logo', () => {
-    const { getByText } = render(<Logo />);
-    expect(getByText('Logo')).toBeInTheDocument();
-  });
-});
