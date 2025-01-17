d39f05796a4720916218cd2469cceba6d15ed1bc
Anderson Miranda <and.miranda1818@gmail.com>
2025-01-17 14:27:08 -0300

    test(TextArea Component): add rendering test

--- src/components/TextArea/textArea.tsx
+++ src/components/TextArea/textArea.tsx
@@ -1,7 +1,6 @@
 import { TextAreaProps } from '@src/types/interfaces';
 import { TextArea_S } from './style';
 
-// desenvolver componente com div
 export default function TextArea({ children }: TextAreaProps) {
   return <TextArea_S>{children}</TextArea_S>;
 }
